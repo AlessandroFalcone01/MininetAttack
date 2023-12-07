d3cda71efeb78f83b0f55e3c29289b337081fa4d
Alessandro Falcone <alefaster25@gmail.com>
2023-12-07 08:59:01 +0000

    General fix

--- net.py
+++ net.py
@@ -6,12 +6,10 @@ from mininet.cli import CLI
 from subprocess import Popen
 from time import sleep
 import csv
-import sys
-import os
 from .topo import CustomTopology
-import pandas as pd
 import matplotlib.pyplot as plt
 from copy import copy
+
 
 class Net:
 
@@ -20,41 +18,41 @@ class Net:
         self.data = {}
 
     def clear_net(self):
-        #Puliamo qualsiasi cosa ci sia in esecuzione su mininet
+        #Clears evrything going on the network
         info('*** Clearing net... ***')
         cmd = "mn -c"
         Popen(cmd, shell=True).wait()
 
     def start_net(self):
-        #Creo la rete
         topo= CustomTopology()
-        controller_ip= '192.168.56.8'
+        controller_ip= '192.168.56.9'
         controller_port= 6633
         self.net = Mininet(topo=topo, controller=lambda name: RemoteController(name, ip=controller_ip, port=controller_port))
         self.net.start()
-        #Abilito il protocollo STP per gli switch
+        #STP protocol
         for i in range (1,4):
             switch_name= f's{i}'
             switch= self.net.get(switch_name)
             switch.cmd(f'ovs-vsctl set bridge switch{switch_name} stp-enable=true')
-        #Testo la rete
+        print("dumoing host connections")
+        dumpNodeConnections(self.net.hosts)
+        #Testing network 
         self.net.pingAll()
         self.net.pingAll()
-        return self.net
 
-    #Fermo la rete
     def stop_net(self):
+        info('*** stopping net ***\n')
         self.net.stop()
 
     #Monitoro la rete (quantità di dati trsmeessi/ricevuti per unità di tempo) e salvo i dati su un file
     def start_monitor(self):
-        info('*** Start monitor\n')
+        info('*** Start monitor ***\n')
         cmd = f"bwm-ng -o csv -T rate -C ',' > {self.tmp} &"
         Popen(cmd, shell=True).wait()
 
     #Smetto di monitorare la rete
     def stop_monitor(self):
-        info('*** Stop monitor\n')
+        info('*** Stop monitor ***\n')
         cmd = "killall bwm-ng"
         Popen(cmd, shell=True).wait()
 
@@ -74,24 +72,31 @@ class Net:
                     self.data[key]['time'] = []
                     self.data[key]['load'] = []
 
+
     #L'host 1 attaccherà 2 e l'host 3 attaccherà 4
     def start_attack(self):
-        info('*** Starting attack ***\n')
+        info('*** starting attack ***')
+
+        #host che attaccheranno
         h1= self.net.get('h1')
         h3= self.net.get('h3')
         ip2= self.net.get('h2').IP()
         ip4= self.net.get('h4').IP()
-        h1.cmd(f"hping3 --flood --udp {ip2} &")
-        h3.cmd(f"hping3 --flood --udp {ip4} &")
+        
+        #attacco
+        h1.cmd(f"hping3 {ip2} --flood --udp &")
+        h3.cmd(f"hping3 {ip4} --flood --udp &")
+        
 
     #Fermo l'attacco
     def stop_attack(self):
-        info('*** Stopping attack ***\n')
+        info('*** stopping attack ***')
         cmd= "killall hping3"
         Popen(cmd, shell=True).wait()
 
     #Creo un garfico a dispersione e lo salvo come immagine
     def plot_latency_for_all_switches(self, output_file):
+        info('*** Printing data ***\n')
         for switch_key in self.data.keys():
             values = self.data[switch_key]
             plt.scatter(values['time'], values['load'], label=f'{switch_key} - Load', alpha=0.5)
@@ -105,22 +110,17 @@ class Net:
     def check_host_connettivity(self, host, target):
         h= self.net.get(host)
         ip_target= self.net.get(target).IP()
-
-        if h.cmd(f"ping {ip_target} &").count('1 packets transmitted, 1 received')>0:
-            return True
-        else:
-            return False
-
+        
+        output= h.cmdPrint(f"ping -c 15 {ip_target}")
+        print(output)
 
     def main(self):
-        self.clear_net()
+        setLogLevel('info')
         self.start_net()
         self.start_monitor()
         sleep(5)    #Durata del monitoring prima dell'attacco
-
-        while self.check_host_connettivity('h1', 'h2') and self.check_host_connettivity('h3', 'h4'):
-            self.start_attack()
-        
+        self.start_attack()
+        sleep(10)    #Durata del monitoring durante l'attacco
         self.stop_attack()
         sleep(5)    #Durata del monitoring dopo aver stoppato l'attacco
         self.stop_monitor()
